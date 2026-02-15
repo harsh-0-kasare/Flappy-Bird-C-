f9a9a29ed0fcc0a9110b21796f733927a223881b
harsh-0-kasare <harshkasare0@gmail.com>
2026-02-15 20:27:39 +0530

    Refactor game classes and add game over handling
    Refactor GameObject, Bird, and Pipe classes; add game over message and background handling.

--- h1.cpp
+++ h1.cpp
@@ -1,224 +1,247 @@
-//Header files 
 #include <iostream>
-#include <graphics.h>//To intialize graphics
+#include <graphics.h>
 #include <conio.h>
-#include <stdlib.h>//Standard Library
-#include <windows.h>//EXCEPTION
+#include <stdlib.h>
+#include <windows.h>
 
-// Declaration of GameObject Class
+using namespace std;
+
+#define SCREEN_W 600
+#define SCREEN_H 400
+#define GROUND_Y (SCREEN_H - 10)
+
+// ---------------- GAME OBJECT ----------------
 class GameObject {
 protected:
-    int x, y;//position:x,y
-    int w, h;//size:w(width of object),h(height of object)
+    int x, y, w, h;
 public:
-//Use of Constructor
     GameObject(int _x, int _y, int _w, int _h) {
         x = _x; y = _y; w = _w; h = _h;
     }
-    int getX() { return x; }//To return value of x
-    int getY() { return y; }//To return value of y
-    int getW() { return w; }//To return value of w
-    int getH() { return h; }//To return value of h
+    int getX() { return x; }
+    int getY() { return y; }
+    int getW() { return w; }
+    int getH() { return h; }
 };
 
-//Declaration of Bird Class
-//Concept of Inheritence
-//Bird Class(Child Class) inherets from GameObject(Parent Class)
+// ---------------- BIRD ----------------
 class Bird : public GameObject {
-    int vy;//vy=VERTICAL VELOCITY
-    int gravity;//gravity=TO GET BIRD FALL DOWN
-    int flap_power;//flap_power=So that the bird wil jump or flap
+    int vy, gravity, flap_power;
 public:
-    Bird(int _x, int _y) : GameObject(_x, _y, 20, 20) {
-        vy = 0;//The Bird should not mobve up or down at the start so velocity=0
-        gravity = 1;//The small Gravity value so that the bird falls down
-        flap_power = -8;//The flap_power is -8 because Negative number means bird goes upwards as we press sapacebar
+    Bird(int _x, int _y) : GameObject(_x,_y,20,20) {
+        vy = 0;
+        gravity = 1;
+        flap_power = -8;
     }
-    //function to make the bird jump
+
     void jump() { vy = flap_power; }
-    //function to update the position of bird
+
     void update() {
-        vy += gravity;//Increases speed because gravity makes the bird fall faster
-        y += vy;//move bird up and down
-        //to stop the bird to move out of the screen
-        if (y < 0) y = 0;//It ensures the bird cannot go above the top edge of the screen.
+        vy += gravity;
+        y += vy;
+        if (y < 0) y = 0;
     }
-     //fuction to draw the bird
+
     void draw() {
         setcolor(YELLOW);
-        setfillstyle(SOLID_FILL, YELLOW);
-        fillellipse(x, y, w / 2, h / 2);//draw circle as bird
+        setfillstyle(SOLID_FILL,YELLOW);
+        fillellipse(x,y,w/2,h/2);
+
+        // eye
+        setcolor(BLACK);
+        setfillstyle(SOLID_FILL,BLACK);
+        fillellipse(x+5,y-3,2,2);
+
+        // wing
+        arc(x-2,y,200,340,8);
     }
 };
-// Pipe Class
+
+// ---------------- PIPE ----------------
 class Pipe : public GameObject {
-    int gap;
-    int speed;
+    int gap, speed;
+    bool passed;
+
 public:
-    Pipe(int _x, int _w, int _gap) : GameObject(_x, 0, _w, 0) {
-        speed = 4;
+    Pipe(int _x,int _w,int _gap) : GameObject(_x,0,_w,0) {
         gap = _gap;
-        reset();//Randomize height
+        speed = 4;
+        reset();
     }
+
     void reset() {
-        y = rand() % 200 + 50;//top pipe height
+        y = rand()%200 + 50;
+        passed = false;
     }
+
     void update() {
         x -= speed;
-        if (x + w < 0) {
-            x = 600;
+        if(x+w < 0) {
+            x = SCREEN_W;
             reset();
         }
     }
-    void draw(int screenh) {
-        setcolor(GREEN);
-        setfillstyle(SOLID_FILL, GREEN);
-        //top pipe
-        bar(x, 0, x + w, y);
-         //bottom pipe
-        bar(x, y + gap, x + w, screenh - 10);
+
+    void increaseDifficulty() {
+        if(speed < 10) speed++;
     }
-    int checkCollision(Bird* bird, int screenh) {
+
+    void draw() {
+        setfillstyle(SOLID_FILL,GREEN);
+
+        // top pipe
+        bar(x,0,x+w,y);
+        bar(x-5,y-20,x+w+5,y);
+
+        // bottom pipe
+        bar(x,y+gap,x+w,GROUND_Y);
+        bar(x-5,y+gap,x+w+5,y+gap+20);
+    }
+
+    bool checkCollision(Bird* bird) {
         int bx = bird->getX();
         int by = bird->getY();
-        int bw = bird->getW();
-        int bh = bird->getH();
-        //Check Horizontal overlap
-        if (bx + bw / 2 > x && bx - bw / 2 < x + w) {
-             //If bird is Not in the gap vertically
-            if (by - bh / 2 < y || by + bh / 2 > y + gap) {
-                return 1;
-            }
+        int r = bird->getW()/2;
+
+        if(bx+r > x && bx-r < x+w) {
+            if(by-r < y || by+r > y+gap)
+                return true;
         }
-        return 0;
+        return false;
     }
-    int passedBy(Bird* bird) {
-        return (bird->getX() == x + w);
+
+    bool isPassed(Bird* bird) {
+        if(!passed && bird->getX() > x+w) {
+            passed = true;
+            return true;
+        }
+        return false;
     }
 };
-// Declaration of Game Class
+
+// ---------------- GAME ----------------
 class Game {
     Bird* bird;
     Pipe* pipe;
-    int screenw, screenh;//screenw-width of the screen
-    //height of screen
-    int score;
-    void* background;  // background image buffer pointer
-    int highscore;
+    int score, highscore;
+    void* background;
+
 public:
-    Game(int w, int h, void* bg) {
-        screenw = w;
-        screenh = h;
-        bird = new Bird(100, h / 2);
-        pipe = new Pipe(w, 40, 100);//Pipe starting from right
-        score = 0;
+    Game(void* bg) {
         background = bg;
-        highscore=0;
+        highscore = 0;
     }
 
     void drawScore() {
-         //Showing the score and updating the score
         char text[20];
-        settextstyle(DEFAULT_FONT, HORIZ_DIR, 5);
-        sprintf(text, "Score: %d", score);
-        outtextxy(10, 10, text);
+        settextstyle(DEFAULT_FONT,HORIZ_DIR,2);
+        sprintf(text,"Score: %d",score);
+        outtextxy(10,10,text);
     }
 
-    // ADDED CODE START: Show "Bird hit the pipe!" message
-    void showGameOverMessage() {
-        settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
-        outtextxy(screenw / 2 - 100, screenh / 2 - 20, (char*)"Bird hit the pipe!");
-        delay(1000); // Show message for 1 second
+    void startScreen() {
+        cleardevice();
+        settextstyle(DEFAULT_FONT,HORIZ_DIR,3);
+        outtextxy(SCREEN_W/2-140,SCREEN_H/2-40,"FLAPPY BIRD");
+
+        settextstyle(DEFAULT_FONT,HORIZ_DIR,1);
+        outtextxy(SCREEN_W/2-120,SCREEN_H/2+10,"Press SPACE to start");
+
+        while(getch()!=' ');
     }
-    // ADDED CODE END
+
+    void showGameOver() {
+        cleardevice();
+        putimage(0,0,background,COPY_PUT);
+
+        settextstyle(DEFAULT_FONT,HORIZ_DIR,2);
+        outtextxy(SCREEN_W/2-60,SCREEN_H/2-20,"GAME OVER");
+
+        char text[40];
+
+        sprintf(text,"Score : %d",score);
+        outtextxy(SCREEN_W/2-60,SCREEN_H/2+20,text);
+
+        sprintf(text,"High Score : %d",highscore);
+        outtextxy(SCREEN_W/2-80,SCREEN_H/2+50,text);
+
+        outtextxy(SCREEN_W/2-120,SCREEN_H/2+90,"Press any key to restart");
+        getch();
+    }
 
     void run() {
-        while (1) {
-            //Check key press so to check input
-            //check if any key is pressed
-            if (kbhit()) {
-                char key = getch();//Read the key is pressed
-                if (key == 27) break;//27=ESC KEY//break stat.for if any spacebar is pressed
-                if (key == ' ') bird->jump();//Bird jumps up
-            }
 
-            bird->update();
-            pipe->update();
+        while(true) {
 
-            cleardevice();//clear oldscreen
-               //to draw bird at new position
-            // Draw the cached background from memory, no lag now
-            putimage(0, 0, background, COPY_PUT);
+            startScreen();
 
-            bird->draw();//Draw ground line at bottom
-            pipe->draw(screenh);
-            drawScore();
-            line(0, screenh - 10, screenw, screenh - 10);
+            score = 0;
+            bird = new Bird(100,SCREEN_H/2);
+            pipe = new Pipe(SCREEN_W,40,100);
 
-            if (bird->getY() > screenh - 20 || pipe->checkCollision(bird, screenh)) {
+            while(true) {
 
-                if(score>highscore){
-                    highscore=score;
+                if(kbhit()) {
+                    char key = getch();
+                    if(key==27) return;
+                    if(key==' ') bird->jump();
                 }
 
-                // ADDED CODE START: Show "Bird hit the pipe!" message first
-                showGameOverMessage();
-                // ADDED CODE END
+                bird->update();
+                pipe->update();
 
-                cleardevice(); // Clear old frame before showing final screen
-                putimage(0, 0, background, COPY_PUT);
+                cleardevice();
+                putimage(0,0,background,COPY_PUT);
 
-                settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
-                outtextxy(screenw / 2 - 60, screenh / 2, (char*)"Game Over!");
+                bird->draw();
+                pipe->draw();
 
-                // ADDED CODE START: Show score slowly after game over message
-                char hstext[30];
-                for(int i = 0; i <= score; i++) {
-                    sprintf(hstext, "Score: %d", i);
-                    settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
-                    outtextxy(screenw /2 - 50, screenh/2 + 30, hstext);
-                    delay(200); // delay to show score slowly
+                // ground
+                setfillstyle(SOLID_FILL,BROWN);
+                bar(0,GROUND_Y,SCREEN_W,SCREEN_H);
+
+                drawScore();
+
+                if(bird->getY()>GROUND_Y || pipe->checkCollision(bird)) {
+
+                    if(score > highscore)
+                        highscore = score;
+
+                    delete bird;
+                    delete pipe;
+
+                    showGameOver();
+                    break;
                 }
-                sprintf(hstext ,"High score:%d", highscore);
-                outtextxy(screenw /2-70,screenh/2+60,hstext);
-                // ADDED CODE END
 
-                getch();
-                break;
+                if(pipe->isPassed(bird)) {
+                    score++;
+                    pipe->increaseDifficulty();
+                }
+
+                delay(40);
             }
-
-            if (pipe->passedBy(bird)) score++;
-            delay(40);
         }
-    }
-     //use of  Destructor
-    ~Game() {
-        delete bird;
-        delete pipe;
     }
 };
 
-// Main Function
+// ---------------- MAIN ----------------
 int main() {
+
     int gd = DETECT, gm;
-    initgraph(&gd, &gm, const_cast<char*>(""));
+    initgraph(&gd,&gm,(char*)"");
 
-    // Load background image once
-    readimagefile("background.bmp", 0, 0, 600, 400);
+    readimagefile("background.bmp",0,0,SCREEN_W,SCREEN_H);
 
-    // Allocate memory and capture the background image into buffer
-    void* background = malloc(imagesize(0, 0, 600, 400));
-    getimage(0, 0, 600, 400, background);
+    void* background = malloc(imagesize(0,0,SCREEN_W,SCREEN_H));
+    getimage(0,0,SCREEN_W,SCREEN_H,background);
 
     cleardevice();
 
-    Game game(600, 400, background);
+    Game game(background);
     game.run();
 
-    free(background);  // Free allocated memory
-
-    getch();
+    free(background);
     closegraph();
     return 0;
 }
-
